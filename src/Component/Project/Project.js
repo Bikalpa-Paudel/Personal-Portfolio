c7b1549dee9299bf53b79d1ab9f203c6725e28ba
Bikalpa <bikalpapaudel53@gmail.com>
2023-01-09 17:55:20 +0545

    more stuff

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -1,10 +1,18 @@
 import "./Project.css"
+import Project1 from "./subProject/Project1"
 
 export default function Project(){
     return(
         <section id="project">
             <h1 className="project-intro">Project</h1>
             <h1 className="project-work">My Best Works</h1>
+            <div className="project1">
+                <Project1 />
+                <Project1 />
+                <Project1 />
+                <Project1 />
+            </div>
+                
         </section>
     )
 }
---
+++ src/Component/Project/subProject/Project1.js
@@ -0,0 +1,9 @@
+import "./Project1.css"
+
+export default function Project1(){
+    return(
+        <>
+            <div className="project-demo"></div>
+        </>
+    )
+}
